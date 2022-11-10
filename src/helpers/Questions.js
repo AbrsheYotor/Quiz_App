d459140fa25fd8ba4b562fd0c770b22c783e9f08
abrsheyotor <abrshefru1427@gmail.com>
2022-11-10 01:49:53 -0800

    quizapp

---
+++ src/helpers/Questions.js
@@ -0,0 +1,45 @@
+export const Questions = [
+  {
+    prompt: "Which of the following method is used to access the state of a component from inside of a member function?",
+    optionA: "this.values.name",
+    optionB: "this.getState()",
+    optionC: "this.target",
+    optionD: "this.state",
+    asnwer: "optionD",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+
+  },
+  {
+    prompt: "Identify the smallest building block of React.JS.",
+    optionA: "Element",
+    optionB: "Body",
+    optionC: "Component",
+    optionD: "none",
+    asnwer: "optionC",
+    
+  },
+  {
+    prompt:" What is ReactJS mainly used for building?",
+    optionA: "Database",
+    optionB: "Integration",
+    optionC: "  User interface",
+    optionD: "Design Platform",
+    asnwer: "optionC",
+    
+  },
+];
